1f064c2b1c9e18afc1cba55b602ea96cea7b0a1b
altostratous <altostratous@gmail.com>
2017-07-09 01:25:00 +0430

    Completed domains

--- domains.sql
+++ domains.sql
@@ -1,11 +1,34 @@
 CREATE DOMAIN email_domain VARCHAR(254)
   CHECK (VALUE ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$');
 
-CREATE DOMAIN nickname_domain VARCHAR(32) NOT NULL;
+CREATE DOMAIN label_domain VARCHAR(32) NULL;
+
+CREATE DOMAIN nickname_domain label_domain;
 
 CREATE DOMAIN password_domain CHAR(32) NOT NULL;
 
-CREATE DOMAIN pic_url_domain VARCHAR(512) NOT NULL
+CREATE DOMAIN url_domain VARCHAR(512) NULL;
+
+CREATE DOMAIN resource_url url_domain;
+
+CREATE DOMAIN pic_url_domain url_domain NOT NULL
   DEFAULT 'http://www.gravatar.com/avatar/00095965ca2e9b81c365d541b9cc73ec?s=40&d=identicon';
 
+CREATE DOMAIN time_setting_domain TIME NULL;
+
 CREATE DOMAIN log_time_domain TIME NOT NULL;
+
+CREATE DOMAIN text_domain VARCHAR(8192) NULL;
+
+CREATE DOMAIN comment_text_domain text_domain NOT NULL;
+
+CREATE DOMAIN title_domain VARCHAR(1024) NOT NULL;
+
+CREATE TYPE activity_type AS (
+  activity_message  comment_text_domain,
+  activity_time     log_time_domain
+);
+
+CREATE DOMAIN path_domain url_domain
+  CHECK (VALUE ~* '^(/[[:space:]A-Za-z0-9._%-]+)+$');
+
