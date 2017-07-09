6c61439f369fb686777be399081a2ab0344e2512
altostratous <altostratous@gmail.com>
2017-07-09 16:00:52 +0430

    Updated documentation and assertions for domain change.

--- assertions.sql
+++ assertions.sql
@@ -7,21 +7,21 @@ CREATE ASSERTION parent_path_validity CHECK (
 
 CREATE ASSERTION predicted_end_time_validity CHECK (
   NOT EXISTS (
-    SELECT * FROM Task WHERE real_end_time < real_time
+    SELECT * FROM Task WHERE real_duration > 0
   )
 )
 
 CREATE ASSERTION predicted_end_time_validity CHECK (
   NOT EXISTS (
-    SELECT * FROM Task WHERE predicted_end_time < predicted_time
+    SELECT * FROM Task WHERE predicted_duration > 0
   )
 )
 
 CREATE ASSERTION reminder_time_validity CHECK (
   NOT EXISTS (
     SELECT * FROM Reminder JOIN Task WHERE
-      Reminder.time > Task.predicted_time OR
-      Reminder.time > Task.real_time
+      Reminder.time > Task.predicted_time + Task.predicted_duration OR
+      Reminder.time > Task.real_time + Task.real_duration
   )
 )
 
