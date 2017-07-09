31ba04a2b07275cc35196adf69cac925115f5aa9
altostratous <altostratous@gmail.com>
2017-07-09 18:42:09 +0430

    Added triggers with documents.

--- triggers.sql
+++ triggers.sql
@@ -1,38 +1,41 @@
-CREATE FUNCTION validate_parent_path() RETURNS TRIGGER AS
+-- procedure to ignore update
+CREATE FUNCTION ignore_manipulation() RETURNS trigger AS $ignore_manipulation$
   BEGIN
-  END
+  RETURN NULL;
+  END;
+$ignore_manipulation$ LANGUAGE plpgsql;
 
-CREATE TRIGGER parent_path_validity
+-- triggers to prevent bad parent folders
+CREATE TRIGGER parent_path_update_validity
   BEFORE UPDATE OF path ON Folder
   FOR EACH ROW
-  WHEN (NOT new.path LIKE (SELECT path FROM Folder WHERE path = new.child_of_path) || '%')
+  WHEN (NOT NEW.path LIKE NEW.child_of_path || '%')
+  EXECUTE PROCEDURE ignore_manipulation();
+  
+CREATE TRIGGER parent_path_insert_validity
+  BEFORE INSERT ON Folder
+  FOR EACH ROW
+  WHEN (NOT NEW.path LIKE NEW.child_of_path || '%')
+  EXECUTE PROCEDURE ignore_manipulation();
 
+-- procedure to ignore bad reminder times
+CREATE FUNCTION ignore_bad_reminder_time() RETURNS trigger AS $ignore_bad_reminder_time$
+	BEGIN
+    IF (SELECT predicted_time + predicted_duration FROM Task WHERE id = NEW.id) < NEW.time THEN
+    	RETURN NULL;
+    END IF;
+    IF (SELECT real_time + real_duration FROM Task WHERE id = NEW.id) < NEW.time THEN
+    	RETURN NULL;
+    END IF;
+    RETURN NEW;
+    END;
+$ignore_bad_reminder_time$ LANGUAGE plpgsql;
 
-CREATE ASSERTION parent_path_validity CHECK (
-  NOT EXISTS (
-    SELECT * FROM Folder JOIN Folder AS Parent
-      WHERE NOT Folder.path LIKE Parent.path || '%'
-  )
-)
-
-CREATE ASSERTION predicted_end_time_validity CHECK (
-  NOT EXISTS (
-    SELECT * FROM Task WHERE real_end_time < real_time
-  )
-)
-
-CREATE ASSERTION predicted_end_time_validity CHECK (
-  NOT EXISTS (
-    SELECT * FROM Task WHERE predicted_end_time < predicted_time
-  )
-)
-
-CREATE ASSERTION reminder_time_validity CHECK (
-  NOT EXISTS (
-    SELECT * FROM Reminder JOIN Task WHERE
-      Reminder.time > Task.predicted_time OR
-      Reminder.time > Task.real_time
-  )
-)
-
-
+-- triggers to prevent invalid reminders
+CREATE TRIGGER reminder_time_insert_validity 
+  BEFORE INSERT ON Reminder
+  FOR EACH ROW EXECUTE PROCEDURE ignore_bad_reminder_time();
+ 
+CREATE TRIGGER reminder_time_update_validity 
+  BEFORE UPDATE OF time ON Reminder
+  FOR EACH ROW EXECUTE PROCEDURE ignore_bad_reminder_time();
